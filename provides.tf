ff954ed217f5fb2e7cd0316360e170e451335a82
dvdgitman <wereking@gmail.com>
2021-08-23 06:27:05 -0700

    Changed Terraform file and syntx

--- provides.tf
+++ provides.tf
@@ -1,8 +1,3 @@
-resource "google_service_account" "default" {
-  account_id   = var.service_account_id
-  display_name = "Service Account"
-}
-
 resource "google_container_cluster" "default" {
   name        = var.cluster_name
   project     = var.project_name
