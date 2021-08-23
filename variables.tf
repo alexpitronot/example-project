b786c12d6be66feccf63ff0d6d2d0b246c09e58e
dvdgitman <wereking@gmail.com>
2021-08-23 07:30:18 -0700

    Changed Terraform file and syntx

--- variables.tf
+++ variables.tf
@@ -33,7 +33,7 @@ variable "project_id" {
 variable "gke_service_account" {
   type = string
   description = "Gke Account name"
-  default = "cluster@int-project-323811.iam.gserviceaccount.com"
+  default = "cluster@terra-k8-323218.iam.gserviceaccount.com"
 }
 
 variable "service_account_id" {
