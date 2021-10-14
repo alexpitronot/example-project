6a047db9e5654535d95602d068040b84df118c96
alexpitronot <87993785+alexpitronot@users.noreply.github.com>
2021-10-14 17:14:22 +0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -33,7 +33,7 @@ variable "project_id" {
 variable "gke_service_account" {
   type = string
   description = "Gke Account name"
-  default = "649441431561@cloudservices.gserviceaccount.com"
+  default = "649441431561-compute@developer.gserviceaccount.com"
 }
 
 variable "service_account_id" {
